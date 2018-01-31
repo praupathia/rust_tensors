972954ecd7bc99c1f29c87f5c90563dd372da672
Michael Summers <praupathia@gmail.com>
2018-01-31 13:05:38 -0500

    working on the closure approach to domain values

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,60 @@
 
 #[macro_use] extern crate frunk_core; // allows us to use the handy hlist! macro
 
-use std::ops::Index;
+use std::fmt;
+use std::fmt::Debug;
+//use std::ops::Index;
 
 //----------------------------------------------------------------------
 
-pub trait IsDimension:
-Index<usize>
-{
-    fn len     (&self) -> usize;
-    fn capacity(&self) -> usize;
+
+#[derive (PartialOrd, Clone, Copy)]
+struct Dim<F> {
+    size:     usize,
+    cap:      usize,
+    value:    F        
 }
 
-struct Dim {
-    size_:     usize,
-    capacity_ : usize,
-}
-
-impl Index<usize> for Dim {
-    type Output = usize;
-    fn index (&self, i: usize) -> &usize {
-        i
+// Is there some annotation to Dim that will permit us to skip
+// implementing these?
+impl <F> Debug for Dim<F> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Dim {{ {}, {} }}", self.size, self.cap)
     }
 }
 
-impl IsDimension for Dim {
-
-    fn capacity(&self) -> usize {
-        self.size_
-    }
-    fn len     (&self) -> usize {
-        self.capacity_
+impl <F> PartialEq for Dim<F> {
+    fn eq(&self, other: &Dim<F>) -> bool {
+        (self.size == other.size)
+            &&
+            (self.cap == other.cap)
     }
 }
 
+//----------------------------------------------------------------------
 //----------------------------------------------------------------------
 
 // DimensionList
 
 fn main() {
+
+    let f = |x: usize| x;
     
-    let h: Hlist!(&str, &str, i32, bool) = hlist!["Joe", "Blow", 30, true];
+    let dim3 = Dim {size:3, cap:3, value:f};
+    
+//    let h: Hlist!(&Dim,&Dim) = hlist![&dim3, &dim3];
+    let h = hlist![&dim3, &dim3];
     
     // We use the Hlist! type macro to make it easier to write
     // a type signature for HLists, which is a series of nested HCons
     // h has an expanded static type of: HCons<&str, HCons<&str, HCons<i32, HCons<bool, HNil>>>>
+
+//    let nrow = dim3[0];
     
     
-    let hlist_pat!(f_name, l_name, age, is_admin) = h;
-    assert_eq!(f_name, "Joe");
-    assert_eq!(l_name, "Blow");
-    assert_eq!(age, 30);
-    assert_eq!(is_admin, true);
+    let hlist_pat!(dim1, dim2) = h;
+    assert_eq!(dim1, &dim3);
+    assert_eq!(dim2, &dim3);
 
     println!("The HList is : {:#?}", h);
     
